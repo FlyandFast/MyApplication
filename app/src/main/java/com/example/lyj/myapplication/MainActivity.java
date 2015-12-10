7787b452ab3265df6c970ed9f70e04a7f059ffb0
mayong3838 <1107324722@qq.com>
2015-12-10 14:13:20 +0800

    gffdshasmjfhdkjd

--- app/src/main/java/com/example/lyj/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lyj/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //niahogfgsgfsaffaffafhjd
+    //niahogfgsgfsaffaffafhjdt5
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
